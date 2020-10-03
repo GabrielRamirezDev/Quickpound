40e4e01ea056d2dc5c37b8cd75bd2580eddf98f8
Gabriel Ramirez <68472070+GabrielRamirezDev@users.noreply.github.com>
2020-10-02 23:47:27 -0400

    Twitter/Instagram style social media with hashtag functionality

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://dbUser:he11oworld@cluster0.lcy7f.mongodb.net/cluster0'
+// looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
